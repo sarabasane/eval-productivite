4e67c64d96c91ac09f38c499047c7544ed443ffa
Alexandre Masson <alexandre.masson@virtuoworks.com>
2022-01-25 12:06:08 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url'
+import cors from 'cors'
+
+const filename=fileURLToPath(import.meta.url)
+const dirname=path.dirname(filename)
+const port=8070
+const host='127.0.0.1'
+
+const app=express()
+app.use(cors())
+app.use(express.static(path.join(dirname,'public'),{
+  setHeaders: function(res,path,stat) {res.set('X-Content-Type-Options','nosniff')}
+}))
+
+app.get('/',(req,res) =>{
+  res.sendFile('index.html',{root: path.join(dirname)},err=>{
+    if (err) throw new Error(err)
+    console.log('Fichier index servi avec succès.')
+  })
+})
+
+app.listen(port,host,() => {console.info(`Server started @ http://${host}:${port}.`)})
